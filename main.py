d3d4f8304dc448b1dc1240e2dcd427fceda24f06
Janelle Sjue <janellesjue@gmail.com>
2018-10-28 23:28:45 -0500

    branch

---
+++ main.py
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+    <body>
+    <h2>Signup</h2>
+    <form method="POST">
+        <input type="text" name="user_id"/>
+        
+
+
+    </body>
+</html> 
