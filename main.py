66b60e60ccffb2cabcbcfe3af7a562bf22b3f0a4
Janelle Sjue <janellesjue@gmail.com>
2018-11-01 16:57:43 -0500

    maybe

--- main.py
+++ main.py
@@ -1,66 +1,59 @@
-import Flask
-
 from flask import Flask, request, redirect, render_template
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 
-app.route('/', Method=['POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == "Post":
-        
+    if request.method == "GET":
+        return render_template('user_signup.html', title='Sign-up')
 
+    if request.method == "POST":
         username = request.form['username']
         email = request.form['email']
         password = request.form['password']
-        password_confirm = request.form['password_confirm']
+        password_confirmation = request.form['password_confirmation']
 
-        email_error = "Please enter a valid email address"
-        username_error="Please enter a valid username (Between 3 and 20 characters with no spaces)"
-        password_error="Please enter a password between 3 and 20 characters"
-        conirm_error="Passwords do not match"
+        email_error = ''
+        username_error = ''
+        password_error = ''
+        confirmation_error = ''
 
-        def no_char(a):
-            if a =='':
+        def is_blank(x):
+            if x == '':
                 return True
-        
-        if no_char(username) or len(username) not in range (3,20) or '' in username:
-            print = username_error
-        if no_char(email):
-            if "@" not in email or "." not in email or len(email) not in range(3,20) or " " in email:
-                return email_error
-        if no_char (password):
-            print = (password_error)
-        elif len (password) not in range(3,20) or " " in password:
-            return password_error
-        elif password != password_confirm:
-            return confirmation_error
-        if not email_error and not username_error and not confirmation_error:
-            return redirect ('/Welcome?username={username}'.format (username=username))
+
+        if is_blank(username) or len(username) not in range(3, 20) or " " in username:
+            username_error = 'please enter a valid username (3 characters or longer & no spaces)'  
+
+        if not is_blank(email):
+            if "@" not in email or "." not in email or len(email) not in range(3, 20) or " " in email:
+                email_error = 'please enter a valid email address'
+
+        if is_blank(password):
+            password_error = 'please enter a password between 3 and 20 characters'
+        elif len(password) not in range(3, 20) or " " in password:
+            password_error = "your password must be between 3 and 20 characters long with no spaces"
+        elif password != password_confirmation:
+            confirmation_error = "passwords don't match"
+
+        if not email_error and not username_error and not password_error and not confirmation_error:
+            return redirect('/welcome?username={username}'.format(username=username))
         else:
-            return render_template('user_signup.html',
-                username=username,
+            return render_template('home-page.html', 
+                username=username, 
                 email=email,
-                username_error=username_error,
                 email_error=email_error,
+                username_error=username_error,
                 password_error=password_error,
                 confirmation_error=confirmation_error,
-                ) 
+                )
 
-@app.route("/Welcome")
-def Welcome():
-    username=request.args.get('username')
-    return render_template('welcome.html',title="Welcome",username=username)   
+@app.route("/welcome")
+def welcome():
+    username = request.args.get('username')
+    return render_template('welcome.html', title='Welcome', username=username)
 
 
-'''template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-@app.route('validate_form', method = ['POST'])
-def validate_id():
-    id = request.form['id']
-    password = request.form['password']
-    userid_error = ""
-    password_error = ""
-
-app.run
-'''
+app.run()
