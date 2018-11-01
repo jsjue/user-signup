b72a26aef981a95df7f574c96b70902c2b065280
Janelle Sjue <janellesjue@gmail.com>
2018-11-01 17:12:41 -0500

    maye

--- main.py
+++ main.py
@@ -4,10 +4,8 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["POST"])
 def index():
-    if request.method == "GET":
-        return render_template('user_signup.html', title='Sign-up')
 
     if request.method == "POST":
         username = request.form['username']
